560b5d9dd193072760b98429278f82baa13c18ad
Paula Soares <sou@paulasoares.me>
2020-10-05 18:40:30 -0300

    Questões de Interpretação e 1a questão de código

---
+++ quinzena4-28set/projeto listajs/index.js
@@ -0,0 +1,29 @@
+// Exercícios de interpretação
+//1. supondo q a cotação digitada pelo usuário seja 5
+//      > R$ 500
+
+//2.    > 165
+//      > TIPO DE INVESTIMENTO INFORMADO INCORRETO!
+
+//3.    > 14
+//      > 6
+//      > 8
+
+//4.    > 1
+//      > 1590
+
+//1.
+const array = [10,20,30,40,50,60,70,80,90,100]
+// ver aula de laços
+// for (const iterator of object) {
+    
+// }
+
+// while {
+
+// }
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
