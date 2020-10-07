e5181c27f7e1a1a1966973533bce95b7dddb5561
Paula Soares <sou@paulasoares.me>
2020-10-06 22:55:44 -0300

    Exercício com os desafios

--- quinzena4-28set/aula15/blog/index.js
+++ quinzena4-28set/aula15/blog/index.js
@@ -1,21 +1,35 @@
-posts = []
-
-function criarPost() {
+function salvaPost() {
+    //Pega os dados do formulário
     const post = {
         titulo: document.getElementById('titulo-post').value,
         autor: document.getElementById('autor-post').value,
-        conteudo: document.getElementById('conteudo-post').value
-    }
+        conteudo: document.getElementById('conteudo-post').value,
+        imagem: document.getElementById("imagem-post").value
+    }  
+    // tenta ver se já tem algo salvo
+    let posts = JSON.parse(localStorage.getItem('posts'));
+    //se não tiver nada salvo, zera o array de posts (tira de null)
+    if (posts === null) {
+        posts = [];
+    } 
+    //adiciona o post
     posts.push(post);
+    //salva o array de posts
+    localStorage.setItem('posts',JSON.stringify(posts));
 
-    document.getElementById('titulo-post').value = "";
-    document.getElementById('autor-post').value = "";
-    document.getElementById('conteudo-post').value = "";
-
-    exibePost(post);
 }
 
-function exibePost(post) {
+function listaPosts() {
+    //pega o array de posts
+    const posts = JSON.parse(localStorage.getItem('posts'))
+    //pega o container dos posts
     const container = document.getElementById('container-de-posts');
-    container.innerHTML += `<h1>${post.titulo}</h1><em>${post.autor}</em><p>${post.conteudo}</p>`
+    for (const p of posts) {
+         container.innerHTML += `<h1>${p.titulo}</h1>
+                            <em>${p.autor}</em>
+                            <img src="${p.imagem}" href="${p.titulo}" />
+                            <p>${p.conteudo}</p>
+                            <hr>`
+    }   
 }
+
